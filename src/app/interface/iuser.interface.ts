69a95696f151319a29e141db5ca8d73f87f01169
Miriam Nieto <miriam.nito726@gmail.com>
2026-03-16 13:33:34 +0100

    Creación de la interface y de los servicios

---
+++ src/app/interface/iuser.interface.ts
@@ -0,0 +1,18 @@
+export interface IUser {
+    _id: string;
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    image: string;
+    password: string;
+}
+
+export interface IUserResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  results: IUser[];
+}
--- src/app/services/user-services.ts
+++ src/app/services/user-services.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser, IUserResponse } from '../interface/iuser.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserServices {
-  
+  private httpClient = inject(HttpClient)
+  private apiUrl: string = "https://peticiones.online/api/users"
+
+  getAll (): Promise<IUserResponse>{
+    return lastValueFrom(this.httpClient.get<IUserResponse>(this.apiUrl))
+  }
+
+  getById (id: number): Promise<IUser> {
+    return lastValueFrom(this.httpClient.get<IUser>(`${this.apiUrl}/${id}`))
+  }
+
 }
